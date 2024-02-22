7a318c9f789874c50074cc3f812b45abb491282c
Madina <madinaodou@gmail.com>
2024-02-22 16:48:01 +0100

    Ajout de méthodes et de filtres à Reservation et TableRes

--- filRougeEquipe/src/main/java/m2i/srpingboot/fil/rouge/equipe/filRougeEquipe/controllers/ReservationController.java
+++ filRougeEquipe/src/main/java/m2i/srpingboot/fil/rouge/equipe/filRougeEquipe/controllers/ReservationController.java
@@ -1,5 +1,7 @@
 package m2i.srpingboot.fil.rouge.equipe.filRougeEquipe.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import m2i.srpingboot.fil.rouge.equipe.filRougeEquipe.entities.Reservation;
+import m2i.srpingboot.fil.rouge.equipe.filRougeEquipe.entities.TableRes;
 import m2i.srpingboot.fil.rouge.equipe.filRougeEquipe.services.ReservationService;
+import m2i.srpingboot.fil.rouge.equipe.filRougeEquipe.services.TableResService;
 
 @RestController
 @CrossOrigin
@@ -22,10 +27,27 @@ import m2i.srpingboot.fil.rouge.equipe.filRougeEquipe.services.ReservationServic
 public class ReservationController {
 	@Autowired
 	private ReservationService rs;
+	@Autowired
+	private TableResService ts;
+	
+//	@GetMapping
+//	public Iterable<Reservation> getAll() {
+//		return rs.getAll();
+//	}
 	
 	@GetMapping
-	public Iterable<Reservation> getAll() {
-		return rs.getAll();
+	public Iterable<Reservation> getReservationsByResto() {
+		return rs.findByRestaurantNom("Le Petit Bistro");
+	}
+	
+	@GetMapping("/tableres")
+	public Iterable<TableRes> getTableRes() {
+		return ts.findByRestaurantId(1);
+	}
+	
+	@GetMapping("/filtred")
+	public List<TableRes> getFiltredTableRes(@RequestParam int reservationId) {
+		return rs.getFilteredTableRes(reservationId);
 	}
 	
 	@GetMapping("/{id}")
--- filRougeEquipe/src/main/java/m2i/srpingboot/fil/rouge/equipe/filRougeEquipe/repositories/ReservationRepository.java
+++ filRougeEquipe/src/main/java/m2i/srpingboot/fil/rouge/equipe/filRougeEquipe/repositories/ReservationRepository.java
@@ -1,9 +1,11 @@
 package m2i.srpingboot.fil.rouge.equipe.filRougeEquipe.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import m2i.srpingboot.fil.rouge.equipe.filRougeEquipe.entities.Reservation;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Integer> {
-
+	List<Reservation> findByRestaurantNom(String nom);
 }
